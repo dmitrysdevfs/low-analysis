67ba98fec8ba85d70c68dfb4a4cac2bd87d28c26
Dmytro Salyakin <d.salyakin@gmail.com>
2026-06-09 18:12:04 +0300

    chore: auto-format before push

--- backend/src/instrument.js
+++ backend/src/instrument.js
@@ -5,10 +5,14 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN,
 
   // Tracing
-  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE ? parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE) : 1.0,
+  tracesSampleRate: process.env.SENTRY_TRACES_SAMPLE_RATE
+    ? parseFloat(process.env.SENTRY_TRACES_SAMPLE_RATE)
+    : 1.0,
 
   // Profiling
-  profilesSampleRate: process.env.SENTRY_PROFILES_SAMPLE_RATE ? parseFloat(process.env.SENTRY_PROFILES_SAMPLE_RATE) : 1.0,
+  profilesSampleRate: process.env.SENTRY_PROFILES_SAMPLE_RATE
+    ? parseFloat(process.env.SENTRY_PROFILES_SAMPLE_RATE)
+    : 1.0,
 
   // Logs
   _experiments: { enableLogs: true },
